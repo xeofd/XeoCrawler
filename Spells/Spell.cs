3e424b3239e77bc808cfc33760ef0cb3e04986e6
Ben Davidson <ben@benfd.uk>
2021-01-18 16:42:10 +0000

    added Spells folder & Spell.cs abstract parent class. Abstracted as spells must only be built if they have specific type

---
+++ Spells/Spell.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XeoCrawler.Spells
+{
+    abstract class Spell
+    {
+
+        // attributes
+        private string splName;
+        private string splType;
+        private int splLevel;
+
+        // constructor
+        public Spell(string splName, string splType, int splLevel)
+        {
+            this.Name = splName;
+            this.Type = splType;
+            this.Level = splLevel;
+        }
+
+        // get & set
+
+        public string Name
+        {
+            get { return this.splName; }
+            set
+            {
+                if (value != "" && value != " ")
+                {
+                    this.splName = value;
+                } else
+                {
+                    this.splName = "{no name}";
+                }
+            }
+        }
+
+        public string Type
+        {
+            get { return this.splType; }
+            set
+            {
+                if (value == "_heal" || value == "_damage" || value == "_enhance")
+                {
+                    this.splType = value;
+                } else
+                {
+                    this.splType = "_damage";
+                }
+            }
+        }
+
+        public int Level
+        {
+            get { return this.splLevel; }
+            set
+            {
+                if (value > 0)
+                {
+                    this.splLevel = value;
+                } else
+                {
+                    this.splLevel = 1;
+                }
+            }
+        }
+
+        // methods
+
+    }
+}
